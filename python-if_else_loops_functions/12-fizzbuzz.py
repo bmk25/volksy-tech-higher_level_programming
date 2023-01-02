765fefba594caf21b38fbab7c1d14568c1332e34
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-02 04:52:07 -0800

    hello

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    if a % 5 == 0 and a % 3 == 0:
+        print("Fizzbuzz", end=" ")
+        continue
+    elif a % 5 == 0:
+        print("Buzz", end=" ")
+        continue
+    elif a % 3 == 0:
+        print("Fizz", end=" ")
+        continue
+    print(a, end=" ")
