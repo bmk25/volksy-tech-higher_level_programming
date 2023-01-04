c1cc0189a29a88fc9c1c842307ae7db0719898d9
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-04 09:14:58 -0800

    hi

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add 
 if __name__  == "__main__":
-    def add(a, b):
-        a = 1
-        b = 2
-        return (a + b)
-    print(a + " + " + b +"{}".format(add(1, 2))
+    a = 1
+    b = 2
+print("{:d} + {:d} = {:d}".format(a,b,add(a, b))
