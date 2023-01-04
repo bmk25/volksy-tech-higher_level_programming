edc30aa013107661a31252ac252a7aede25be098
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-04 09:07:10 -0800

    hello

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add 
 if __name__  == "__main__":
-    a = 1
-    b = 2
-    print(a + " + " + b + " = {}".format(add(a,b))
+    def add(a, b):
+        a = 1
+        b = 2
+        return (a + b)
+    print(a + " + " + b +"{}".format(add(1, 2))
