e54446631a572ba2ff79f659b9f9219181acc40d
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-05 10:30:39 -0800

    jhi

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     else:
         my_list.pop(idx)
-        my_list.insert(idx,element)
+        my_list.insert(idx, element)
         return my_list
