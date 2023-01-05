5abc2f3186fa582ddab02df5b69abdf30fedcff1
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-05 12:40:18 -0800

    6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/bin/usr/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for x in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][x]), end=" ")
+        print()
