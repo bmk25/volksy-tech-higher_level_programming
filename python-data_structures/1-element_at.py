fcf9dd36d27337eb1bca91a800bbc8dc421a2def
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-05 03:49:52 -0800

    ji

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if  idx < 0:
-        print(None)
-    elif idx > len(my_list):
-        print(None)
+        return None
+    elif idx > (len(my_list)-1):
+        return None
     else:
-        print(my_list[idx])
+        return my_list[idx]
 #print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
