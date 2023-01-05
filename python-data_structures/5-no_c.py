8c9405f5d59132d5429ebc105bc5bc2b62ea874c
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-05 12:22:39 -0800

    4

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    str=""
     for i in my_string:
         if i == ('c' or 'C'):
-            print("{}".format(""),end="")
+            print("{}".format(""), end="")
+            # str=str+""
         else:
-            print("{}".format(i),end="")
-    return my_string
+            print("{}".format(i), end="")
+            # str=str+ ("{}".format(i))
+    # return str
