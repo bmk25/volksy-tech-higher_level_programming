7fbf2dcf93d258b0a8bf18033a32f034a3b05e82
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-08 06:03:23 -0800

    hi

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    ls1=list(tuple_a)
-    ls2=list(tuple_b)
-    ls3=[]        
+def add_tuple(tuple_a = (), tuple_b = ()):
+    ls1 = list(tuple_a)
+    ls2 = list(tuple_b)
+    ls3 = []
     for i in range(2):
         ls1.append(0)
         ls2.append(0)
-        ls3.append(ls1[i]+ ls2[i] if ls2 !=0 else 0)
-    tup1=tuple(ls3)
+        ls3.append(ls1[i] + ls2[i])
+    tup1 = tuple(ls3)
     return tup1
