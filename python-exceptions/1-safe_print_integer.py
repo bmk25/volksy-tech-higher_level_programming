4d0fd564e9d72ce6ee2735b218bfdb91ddf1d08b
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-09 15:06:24 -0800

    hi

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if "{:d}".format(value) :
+        if "{:d}".format(value):
             print(value)
             return True
     except:
