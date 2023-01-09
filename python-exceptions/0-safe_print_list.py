1a1f6c33da5fbe55d8713daf651df6dde9a0b303
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-09 14:44:49 -0800

    hi

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x = 0):
+def safe_print_list(my_list=[], x=0):
         try:
             y = 0
             for i in range(x):
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x = 0):
                 y += 1
             print()
             return y
-        except (TypeError,IndexError):
+        except (TypeError, IndexError):
             print()
             return y
