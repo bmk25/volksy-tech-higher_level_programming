74187fe2c00cb9051ffcce1b7002682c79d5841a
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-09 16:50:29 -0800

    hi

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside reslt :{:d}".format(a/b))
+        result = a / b
+        return result
     except ZeroDivisionError:
-        print("Inside reslt :{}".format(None))
-        return None 
+        result = None
+        return result 
     finally:
-        try:
-            return a / b
-        except ZeroDivisionError:
-            return None
+        print("Inside result : {}".format(result))
