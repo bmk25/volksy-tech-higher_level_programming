b3f304ef13571587a05b14522c9efc345c0385fd
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-09 16:39:12 -0800

    hi

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside reslt :",a/b)
+        print("Inside reslt :{:d}".format(a/b))
     except ZeroDivisionError:
-        print("Inside reslt : None")
+        print("Inside reslt :{}".format(None))
         return None 
     finally:
         try:
