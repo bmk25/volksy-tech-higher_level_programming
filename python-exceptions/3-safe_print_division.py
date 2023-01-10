55e8491f2348b886fa45dceb9ac95d70ee9bd9e8
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-09 16:52:06 -0800

    hi

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,6 @@ def safe_print_division(a, b):
         return result
     except ZeroDivisionError:
         result = None
-        return result 
+        return result
     finally:
         print("Inside result: {}".format(result))
