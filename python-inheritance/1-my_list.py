2e80d70477faaebfc49b239ba084fe91665505b4
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-15 09:58:59 -0800

    hell 0

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin.python3
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """built-in list class."""
+
+    def print_sorted(self):
+        print(sorted(self))
