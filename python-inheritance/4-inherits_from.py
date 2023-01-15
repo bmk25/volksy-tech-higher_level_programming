4d08f3ac78b92fa51b8b283f24d5abf6d237b472
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-15 10:38:09 -0800

    hel

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""class-checking function."""
+""" class-checking function."""
 
 
 def is_kind_of_class(obj, a_class):
     """this is 4th """
+
     if isinstance(obj, a_class):
         return True
     return False
