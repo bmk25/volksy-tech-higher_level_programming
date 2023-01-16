fdf1bffbad94bed1a14c6af4e2a675b7f2c15774
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-15 21:30:10 -0800

    jheL

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-""" hello 8 """
+"""Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-Rectangle = __import__('8-rectangle').Rectangle
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
