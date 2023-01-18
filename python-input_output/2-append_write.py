d755f77028f7d5416a58d8b800655fd75b671368
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-18 03:48:19 -0800

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+""" append the new file"""
 
+def append_write(filename="", text=""):
+    """ append new text and find the no of characters"""
+
+    with open(filename, "a+", encoding="utf=8") as f:
+        f.write(text)
+        return (len(text))
