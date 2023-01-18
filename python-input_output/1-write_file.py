cd6e352051eb1dae578439d9d6404171ad37ec6c
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-18 03:17:55 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
+    """ no of characters in a file """
+
     with open(filename, "w+", encoding =" utf -8") as f:
         f.write(text)
         return len(text)
