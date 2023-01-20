3040db43d3850b89c5f2010c5eecfee88ec33f14
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-19 20:39:23 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -6,7 +6,6 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -17,3 +16,8 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for k, v in json.items():
+            setattr(self, k, v)
