35412cb6e7db438af59c8e144dc2380f0bd359f3
bmk25 <madhukiranb@brightcomgroup.com>
2023-01-23 01:58:26 -0800

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,9 +6,10 @@ class Base:
     ''' this about the id'''
 
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
-            Base.__nb_objects = + 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
