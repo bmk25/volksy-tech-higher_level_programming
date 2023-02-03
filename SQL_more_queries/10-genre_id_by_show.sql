ed30fc8c9c09ca0fa927dc21ac0a25497171c154
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-02 23:55:03 -0800

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- 10 
 SELECT tv_shows.title,tv_show_genres.genre_id
 FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
-ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC,tv_show_genres.genre_id ASC;
