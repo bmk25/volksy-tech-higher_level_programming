4919d0807c18fb369b32d1bd5f8710a503195c90
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-03 22:22:26 -0800

    13-count_shows_by_genre.sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- 13th
-SELECT g.name AS genre,count(sg.genre_id) as number_of_shows
+SELECT g.name AS genre,COUNT(sg.genre_id) as number_of_shows
 FROM tv_show_genres AS sg INNER JOIN tv_genres AS g ON genre_id = id
 GROUP BY genre_id
 ORDER BY number_of_shows DESC;
