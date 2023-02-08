856ba7b26d795020410e894141de824e0c467648
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 02:03:53 -0800

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python3
-''' task 1  '''
-from sys import argv
-import MySQLdb
-
-if __name__ == '__main__':
-	mydb = MySQLdb.connect(host= "argv[0]",user="argv[1]",passwd="argv[2]",database="hbtn_0e_0_usa")
+#/usr/python
+""" 0 task """
+if __name__ == "__main__" :
+        from sys import argv
+	import MySQLdb
+	mydb = MySQLdb.connect(host= "argv[0]",user="argv[1]",passwd="argv[2]",db="hbtn_0e_0_usa")
 	s = mydb.cursor()
 	s.execute("select * from states")
 	for i in s:
 		print(i)
-        s.close()
-        mydb.close() 
+	s.close()
+        mydb.close()
