ac54707111eeac7b7a40de6830501450f4225806
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 01:53:18 -0800

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' o task '''
-if __name__ == __main__:
+if __name__ == '__main__':
 	from sys import argv
 	import MySQLdb
 	mydb = MySQLdb.connect(host= "argv[0]",user="argv[1]",passwd="argv[2]",database="hbtn_0e_0_usa")
