f1be3107e494ff7d9f13045c7a5fb2ce42a7b31f
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 21:24:19 -0800

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 02 task """
+from sys import argv
+import MySQLdb
+
+if __name__ ==__main__:
+    mydb = MySQLdb.connect(user= argv[1],passwd=argv[2],db=argv[3])
+    s = mydb.cursor()
+    a = s.execute (f"SELECT * FROM states WHERE name = {argv[4]}")
+    for i in a:
+        print(i)
+    mydb.close()
