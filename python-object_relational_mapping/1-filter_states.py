09e7b3da011302dc4c91ff39108f7d9c1e96a4ed
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 20:10:50 -0800

    01

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    cnt = MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3])
+    cnt = MySQLdb.connect(host=argv[0], user=argv[1], passwd=argv[2], db=argv[3])
     s = cnt.cursor()
     a = "SELECT id,name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     s.execute(a)
