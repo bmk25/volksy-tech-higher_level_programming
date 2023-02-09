2f4148bb8915f957f32ddbcd62bb3afb0b87a2f1
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 23:29:35 -0800

    3-my_safe_filter_states.p

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+"""
+safe sql injection
+"""
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[4])
+    c = db.cursor()
+    txt1 = f"{argv[4]}"
+    sql = "SELECT * FROM states WHERE NAME BINARY LIKE @0 ORDER BY id ASC"
+    c.execute(sql,txt1)
+    db.close()
