31742e57f0145ef67360a5b565a5e20c92df3796
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-08 21:44:57 -0800

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" 1st  """
+'''
+this for name matches 
+'''
 
 
 from sys import argv
@@ -7,7 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
     mydb = MySQLdb.connect(user= argv[1],passwd=argv[2],db=argv[3])
     s = mydb.cursor()
-    a = s.execute (f"SELECT * FROM states WHERE name = {argv[4]}")
+    a = s.execute (f"SELECT * FROM states WHERE name = {argv[4]} ORDER BY id ASC")
     for i in a:
         print(i)
     mydb.close()`
