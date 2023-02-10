d42839f0f0b02efdd5c9f7913859090ed0e09322
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-09 20:43:46 -0800

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-safe sql injection
-"""
-from sys import argv
-import MySQLdb
-
+"""importing sql to pythondatabase"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE NAME = %(name)s
-               ORDER BY id ASC",{'name' : argv[4]})
-    row = c.fetchall()
-    for i in row:
-        print(i)
-    c.close()
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(" SELECT * FROM states WHERE name = %(name)s \
+            ORDER BY states.id ASC", {'name': argv[4]})
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
