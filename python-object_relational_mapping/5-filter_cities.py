11ac9020d78f255d163773689fdf43af737068aa
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-10 02:24:09 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,7 +5,7 @@ safe sql injection
 from sys import argv
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306 )
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM cities\
                 INNER JOIN states\
