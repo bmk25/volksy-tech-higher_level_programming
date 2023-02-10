9b6f0afa06b2c7c606c8cfd580db9531e8ff6391
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-09 20:20:51 -0800

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,9 +8,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    txt1 =argv[4]
-    sql = "SELECT * FROM states WHERE NAME LIKE BINARY %s ORDER BY id ASC"
-    c.execute(sql,txt1)
+    sql = "SELECT * FROM states WHERE NAME = %(name)s ORDER BY id ASC",{'name' :argv[4]}
+    c.execute(sql)
     for i in c.fetchall:
         print(i)
+    c.close()
     db.close()
