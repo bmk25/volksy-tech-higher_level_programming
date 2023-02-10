b379d00766a503e28e77ad02f536bb929bbb61c8
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-10 01:20:12 -0800

    5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#/usr/bin/python3
+"""
+safe sql injection
+"""
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306 )
+    c = db.cursor()
+    sql = "select cities.name\
+           from cities inner join states on cities.state_id = cities.id\
+           where state.name = %(name)s ",{'name': argv[4]}
+    c.execute(sql)
+    row = c.fetchall()
+    [print(i,end=",") for i in row]
+    c.close()
+    db.close()
