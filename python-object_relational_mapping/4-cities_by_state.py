70a0f088720895814b350374d8bd2052b8a2631b
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-09 21:40:19 -0800

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#/usr/bin/python3
+"""
+safe sql injection
+"""
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306 )
+    c = db.cursor()
+    sql = "select cities from cities order by id"
+    c.execute(sql)
+    for i in c.fetchall():
+        print(i)
+    c.close()
+    db.close()
+
