36d3b3763aa38e4910dad3d73ee48e23daeb7a16
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-09 19:57:56 -0800

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+#/usr/bin/python3
 """
 safe sql injection
 """
 from sys import argv
 import MySQLdb
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[4])
     c = db.cursor()
-    txt1 = argv[4]
-    sql = "SELECT * FROM states WHERE NAME LIKE BINARY %s ORDER BY id ASC"
+    txt1 =argv[4]
+    sql = "SELECT * FROM states WHERE NAME BINARY LIKE %s ORDER BY id ASC"
     c.execute(sql,txt1)
-    for i in c.fetchall():
+    for i in c.fetchall:
         print(i)
     db.close()
