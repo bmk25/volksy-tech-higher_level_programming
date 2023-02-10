c989c709f714e78a9e9958b6ce9c1eacc1245592
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-10 02:14:10 -0800

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306 )
     c = db.cursor()
     sql = "select cities.name\
-           from cities\
-           where state_id = (select id from states where states.name = %(name)s)",\
-           {'name': argv[4]}
+           from cities \
+           where state_id = (select id\
+           from states where states.name = %(name)s)",{'name': argv[4]}
     c.execute(sql)
     row = c.fetchall()
-    [print(i, end=",") for i in row]
+    [print(i[0], end=",") for i in row]
     c.close()
     db.close()
