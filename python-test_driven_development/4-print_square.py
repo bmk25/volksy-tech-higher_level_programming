a5a8f52d96a72b86307a30d070cc2ea2abe52de5
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-13 01:54:28 -0800

    3-say_my_name.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' hello'''
+
+def print_square(size):
+    '''about the square'''
+    if isinstance(size,int) is False:
+        raise TypeError("size must be an integer")
+    if size<0 or isinstance(size, float):
+        raise TypeError("size must be >= 0")
+    if size == 0:
+        return 
+    for i in range(size):
+        for j in range(size):
+            print("#",end="")
+        print()
