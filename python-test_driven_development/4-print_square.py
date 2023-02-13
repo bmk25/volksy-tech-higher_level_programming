4bde3117807aa7bf59b1bc942412555c3c387fe6
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-13 02:08:10 -0800

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,6 +4,7 @@
 def print_square(size):
     '''about the square'''
 
+
     if isinstance(size, int) is False:
         raise TypeError("size must be an integer")
     if size < 0 or isinstance(size, float):
