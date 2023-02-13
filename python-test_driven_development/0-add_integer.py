1969d4115f0a73ec0680615ed0a1d7e1181b2b5b
bmk25 <madhukiranb@brightcomgroup.com>
2023-02-12 23:01:43 -0800

    0-add_integer.txt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@
 
 def add_integer(a, b=98):
     """ hello"""
+
     if isinstance(a,(int,float)) is False:
         raise TypeError("a must be an integer")
     if isinstance(b,(int,float)) is False:
